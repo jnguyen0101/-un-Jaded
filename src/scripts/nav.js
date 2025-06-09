f6d761b812d364b5fba8667ec0bc23f3b6ff5e24
jnguyen0101 <nguyenjenny01012006@gmail.com>
2025-06-09 14:22:21 -0500

    test

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -57,7 +57,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 menu.classList.add("show-touch");
                 activeMenu = menu;
             } else {
-                menu.classList.remove("show-touch");
                 activeMenu = null;
             }
         });
