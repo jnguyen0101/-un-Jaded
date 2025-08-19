ac501c93147d89cdfaf3b4695332d4c556832fa1
jnguyen0101 <nguyenjenny01012006@gmail.com>
2025-08-18 22:32:43 -0500

    test

--- src/scripts/wiUJ-modal.js
+++ src/scripts/wiUJ-modal.js
@@ -5,6 +5,8 @@ function openModal(id) {
     setTimeout(() => {
         modal.classList.add('show');
     }, 10);
+
+    document.body.classList.add('no-scroll');
 }
 
 function closeModal(id) {
@@ -14,6 +16,8 @@ function closeModal(id) {
     setTimeout(() => {
         modal.style.display = 'none';
     }, 400);
+
+    document.body.classList.remove('no-scroll');
 }
 
 // Close if clicked outside content
